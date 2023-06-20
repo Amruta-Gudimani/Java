33a1a0a3199db3c92e4a22db72421a8dd342e19f
Amruta-Gudimani <137047235+Amruta-Gudimani@users.noreply.github.com>
2023-06-20 12:12:36 +0530

    Add files via upload

---
+++ variableclass.java
@@ -0,0 +1,196 @@
+public class VariableDeclaration {
+    public static void main(String[] args) {
+        // Byte variables
+        byte age = 25;
+        byte numberOfChildren = 2;
+        byte temperature = 32;
+        byte score = 90;
+        byte quantity = 5;
+        byte pages = 100;
+        byte distance = 50;
+        byte population = 100;
+        byte weight = 70;
+        byte height = 120;
+
+        // Short variables
+        short numberOfStudents = 500;
+        short roomCapacity = 100;
+        short temperatureRange = 1000;
+        short maxScore = 100;
+        short productQuantity = 50;
+        short shelfCapacity = 30;
+        short distanceCovered = 1000;
+        short populationDensity = 200;
+        short containerSize = 500;
+        short resolution = 1080;
+
+        // Integer variables
+        int numberOfBooks = 1000;
+        int populationCount = 1000000;
+        int bankBalance = 500000;
+        int phoneNumber = 1234567890;
+        int temperatureInCelsius = 25;
+        int maxCapacity = 100;
+        int distanceInMeters = 5000;
+        int weightInGrams = 75000;
+        int heightInCentimeters = 180;
+        int employeeCount = 50;
+
+        // Long variables
+        long populationCount = 1000000000L;
+        long bankBalance = 5000000000L;
+        long phoneNumber = 123456789012345L;
+        long distanceInMeters = 500000L;
+        long weightInGrams = 750000L;
+        long heightInCentimeters = 180000L;
+        long fileStorageSize = 1000000000000L;
+        long revenue = 50000000000L;
+        long elapsedTime = 10000000000L;
+        long numberOfStars = 10000000000L;
+
+        // Float variables
+        float temperature = 25.5f;
+        float weight = 68.9f;
+        float height = 175.3f;
+        float price = 9.99f;
+        float bankBalance = 5000.75f;
+        float distanceInKm = 10.5f;
+        float area = 25.5f;
+        float speed = 45.6f;
+        float volume = 10.3f;
+        float age = 35.2f;
+
+        // Double variables
+        double pi = 3.14159;
+        double euler = 2.71828;
+        double gravity = 9.8;
+        double temperature = 25.5;
+        double bankBalance = 5000.75;
+        double distanceInKm = 10.5;
+        double area = 25.5;
+        double speed = 45.6;
+        double volume = 10.3;
+        double age = 35.2;
+
+        // Character variables
+        char grade = 'A';
+        char category = 'B';
+        char symbol = '#';
+        char firstInitial = 'J';
+        char lastInitial = 'D';
+        char currency = '$';
+        char option = 'Y';
+        char digit = '9';
+        char punctuation = '.';
+        char specialCharacter = '@';
+
+        // Boolean variables
+        boolean isEmployed = true;
+        boolean hasChildren = false;
+        boolean isRaining = true;
+        boolean hasPermission = false;
+        boolean isActive = true;
+        boolean isLoggedIn = false;
+        boolean isSuccessful = true;
+        boolean isReady = false;
+        boolean isAvailable = true;
+        boolean isLate = false;
+
+        // Print the values of variables
+        System.out.println("Byte variables:");
+        System.out.println("Age: " + age);
+        System.out.println("Number of Children: " + numberOfChildren);
+        System.out.println("Temperature: " + temperature);
+        System.out.println("Score: " + score);
+        System.out.println("Quantity: " + quantity);
+        System.out.println("Pages: " + pages);
+        System.out.println("Distance: " + distance);
+        System.out.println("Population: " + population);
+        System.out.println("Weight: " + weight);
+        System.out.println("Height: " + height);
+
+        System.out.println("\nShort variables:");
+        System.out.println("Number of Students: " + numberOfStudents);
+        System.out.println("Room Capacity: " + roomCapacity);
+        System.out.println("Temperature Range: " + temperatureRange);
+        System.out.println("Max Score: " + maxScore);
+        System.out.println("Product Quantity: " + productQuantity);
+        System.out.println("Shelf Capacity: " + shelfCapacity);
+        System.out.println("Distance Covered: " + distanceCovered);
+        System.out.println("Population Density: " + populationDensity);
+        System.out.println("Container Size: " + containerSize);
+        System.out.println("Resolution: " + resolution);
+
+        System.out.println("\nInteger variables:");
+        System.out.println("Number of Books: " + numberOfBooks);
+        System.out.println("Population Count: " + populationCount);
+        System.out.println("Bank Balance: " + bankBalance);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Temperature in Celsius: " + temperatureInCelsius);
+        System.out.println("Max Capacity: " + maxCapacity);
+        System.out.println("Distance in Meters: " + distanceInMeters);
+        System.out.println("Weight in Grams: " + weightInGrams);
+        System.out.println("Height in Centimeters: " + heightInCentimeters);
+        System.out.println("Employee Count: " + employeeCount);
+
+        System.out.println("\nLong variables:");
+        System.out.println("Population Count: " + populationCount);
+        System.out.println("Bank Balance: " + bankBalance);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Distance in Meters: " + distanceInMeters);
+        System.out.println("Weight in Grams: " + weightInGrams);
+        System.out.println("Height in Centimeters: " + heightInCentimeters);
+        System.out.println("File Storage Size: " + fileStorageSize);
+        System.out.println("Revenue: " + revenue);
+        System.out.println("Elapsed Time: " + elapsedTime);
+        System.out.println("Number of Stars: " + numberOfStars);
+
+        System.out.println("\nFloat variables:");
+        System.out.println("Temperature: " + temperature);
+        System.out.println("Weight: " + weight);
+        System.out.println("Height: " + height);
+        System.out.println("Price: " + price);
+        System.out.println("Bank Balance: " + bankBalance);
+        System.out.println("Distance in Km: " + distanceInKm);
+        System.out.println("Area: " + area);
+        System.out.println("Speed: " + speed);
+        System.out.println("Volume: " + volume);
+        System.out.println("Age: " + age);
+
+        System.out.println("\nDouble variables:");
+        System.out.println("Pi: " + pi);
+        System.out.println("Euler: " + euler);
+        System.out.println("Gravity: " + gravity);
+        System.out.println("Temperature: " + temperature);
+        System.out.println("Bank Balance: " + bankBalance);
+        System.out.println("Distance in Km: " + distanceInKm);
+        System.out.println("Area: " + area);
+        System.out.println("Speed: " + speed);
+        System.out.println("Volume: " + volume);
+        System.out.println("Age: " + age);
+
+        System.out.println("\nCharacter variables:");
+        System.out.println("Grade: " + grade);
+        System.out.println("Category: " + category);
+        System.out.println("Symbol: " + symbol);
+        System.out.println("First Initial: " + firstInitial);
+        System.out.println("Last Initial: " + lastInitial);
+        System.out.println("Currency: " + currency);
+        System.out.println("Option: " + option);
+        System.out.println("Digit: " + digit);
+        System.out.println("Punctuation: " + punctuation);
+        System.out.println("Special Character: " + specialCharacter);
+
+        System.out.println("\nBoolean variables:");
+        System.out.println("Is Employed: " + isEmployed);
+        System.out.println("Has Children: " + hasChildren);
+        System.out.println("Is Raining: " + isRaining);
+        System.out.println("Has Permission: " + hasPermission);
+        System.out.println("Is Active: " + isActive);
+        System.out.println("Is Logged In: " + isLoggedIn);
+        System.out.println("Is Successful: " + isSuccessful);
+        System.out.println("Is Ready: " + isReady);
+        System.out.println("Is Available: " + isAvailable);
+        System.out.println("Is Late: " + isLate);
+    }
+}
