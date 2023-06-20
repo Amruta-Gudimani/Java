30aeae31476d8b22c32d97a5725a426652b4773f
Amruta-Gudimani <137047235+Amruta-Gudimani@users.noreply.github.com>
2023-06-20 21:41:22 +0530

    Update ArrayClass.java

--- ArrayClass.java
+++ ArrayClass.java
@@ -13,25 +13,33 @@ public class ArrayClass {
         String[] countries = {"India", "USA", "France", "Japan", "UK"};
 
         // Creating 10 arrays with the new keyword
-        int[] ages = new int[5];
-        int[] names = new int[5];
-        String[] salaries = new String[5];
-        String[] isMarried = new String[5];
-        double[] cities = new double[5];
-        int[] scores = new int[5];
-        double[] fruits = new double[5];
-        boolean[] weights = new boolean[5];
-        String[] isEmployed = new String[5];
-        boolean[] countries = new boolean[5];
+        int[] ages = new int[4];
+        int[] names = new int[4];
+        String[] salaries = new String[4];
+        String[] isMarried = new String[3];
+        double[] cities = new double[2];
+        int[] scores = new int[2];
+        double[] fruits = new double[3];
+        boolean[] weights = new boolean[4];
+        String[] isEmployed = new String[2];
+        boolean[] countries = new boolean[3];
 
         // Changing the values of 5 elements using a for loop
         for (int i = 0; i < 5; i++) {
-            ages[i] += 5; 
-            salaries[i] *= 1.1;
-            scores[i] -= 5; 
-            weights[i] += 2.5; 
-            isEmployed[i] = !isEmployed[i]; 
+            salaries[i] *= 1.1;  
         }
+		for (int i = 0; i < 5; i++) {
+			ages[i] += 5; 
+		}
+		for (int i = 0; i < 5; i++) {
+			scores[i] -= 5;
+		}
+		for (int i = 0; i < 5; i++) {
+			weights[i] += 2.5;
+		}
+		for (int i = 0; i < 5; i++) {
+			  isEmployed[i] = !isEmployed[i];
+		}	 
 
         // Printing the updated values of the arrays
         System.out.println("Updated values of meaningful arrays:");
